e9dcfc381bcdec0e9ef17b9b5bf09d866fe2d6db
rssXX <31318078+rssXX@users.noreply.github.com>
2024-09-19 17:55:13 +0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.jsx'),
+        output: {
+            filename: '[name].[contenthash].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+            publicPath: "/"
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html')
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: {
+                                    namedExport: false,
+                                    exportLocalsConvention: 'as-is',
+                                    localIdentName: env.mode === 'development'? '[path][name]__[local]' : '[hash:base64]'
+                                }
+                            }
+                        },
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                    },
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.jsx', '.js']
+        },
+        devServer: {
+            watchFiles: path.join(__dirname, 'src'),
+            port: 9000,
+            hot: true,
+            compress: true,
+            historyApiFallback: true,
+            static: path.resolve(__dirname, 'public')
+        },
+    }
+};
